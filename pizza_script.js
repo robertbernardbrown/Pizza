6e55e000b5f40455002275cd1a4d5ebdd8cd0570
Bob Brown <robertbernardbrown@gmail.com>
2017-09-09 23:38:21 -0400

    Updated HTML, created signup page, created form on sign up page, created script file with button action

---
+++ pizza_script.js
@@ -0,0 +1,7 @@
+//switch from main to sign-up page
+
+var indexClick = document.getElementsByClassName("pizza_image");
+
+function pizzaClick () {
+  indexClick.onlick = location.href = "signup.html";
+} 
