720a8f5f1ccca063ede6f35881de0c11feb24818
Bob Brown <robertbernardbrown@gmail.com>
2017-09-23 13:00:19 -0400

    Create templates of html files using flask

--- pizza_backend.py
+++ pizza_backend.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 
-@app.route('/input')
-def home():
-		return render_template('input.html')
+app.secret_key = "my precious" 
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,9 +22,37 @@ def login():
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
 			error = 'invalid credentials. Please try again.'
 		else:
-			return redirect(url_for('home'))
+			session['logged_in'] = True
+			flash('you were just logged in')
+			return redirect(url_for('login'))
 	return render_template('login.html', error=error)
 
+@app.route('/input')
+@login_required
+def input():
+		return render_template('input.html')
+
+@app.route('/choice')
+@login_required
+def choice():
+		return render_template('choice.html')
+
+@app.route('/finally')
+@login_required
+def final():
+		return render_template('finally.html')
+
+@app.route('/signup')
+@login_required
+def signup():
+		return render_template('signup.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+	session.pop('logged_in', None)
+	flash('You were just logged out')
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
 		app.run(debug=True)
