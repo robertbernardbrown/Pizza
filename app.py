7f52a5800ce8230e333e5d0b9f6368fdebff5d2c
Bob Brown <robertbernardbrown@gmail.com>
2017-10-16 20:26:50 -0400

    Finish hashing passwords and adding flask-login

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -12,12 +14,19 @@ Bootstrap(app)
 app.secret_key = "my precious"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/robertbernardbrown/Desktop/GitHub2/Pizza/database.db'
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -42,7 +51,14 @@ def login_required(f):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect (url_for('home'))
 
+        return '<h> Invalid username or password </h>'
     return render_template('login.html', form=form)
 
 @app.route('/')
@@ -69,7 +85,8 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
@@ -80,9 +97,9 @@ def signup():
 @app.route('/logout')
 @login_required
 def logout():
-	session.pop('logged_in', None)
+	logout_user()
 	flash('You were just logged out')
-	return redirect(url_for('login'))
+	return redirect(url_for('home'))
 
 if __name__ == '__main__':
 		app.run(debug=True)
